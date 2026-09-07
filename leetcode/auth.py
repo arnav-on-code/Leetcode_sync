bdc38d2c571a77d802dc9ff31fbdb02a4f50311b
Arnav Raj <157286373+arnav-on-code@users.noreply.github.com>
2026-09-07 22:35:13 +0530

    Fix LeetCode submission authentication

--- leetcode/auth.py
+++ leetcode/auth.py
@@ -4,26 +4,31 @@ from config.settings import Config
 
 
 class LeetCodeAuth:
-
     def __init__(self):
         self.session = Session()
 
-        self.session.headers.update(
-            {
-                "User-Agent": Config.USER_AGENT,
-                "Referer": "https://leetcode.com/",
-                "Origin": "https://leetcode.com",
-                "Content-Type": "application/json",
-            }
-        )
+        self.session.headers.update({
+            "User-Agent": Config.USER_AGENT,
+            "Referer": "https://leetcode.com/",
+            "Origin": "https://leetcode.com",
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        })
 
-        self.session.cookies.update(
-            {
-                "LEETCODE_SESSION": Config.LEETCODE_SESSION,
-                "csrftoken": Config.CSRF_TOKEN,
-            }
-        )
+        # Authentication cookies
+        if Config.LEETCODE_SESSION:
+            self.session.cookies.set(
+                "LEETCODE_SESSION",
+                Config.LEETCODE_SESSION,
+                domain=".leetcode.com"
+            )
+
+        if Config.CSRF_TOKEN:
+            self.session.cookies.set(
+                "csrftoken",
+                Config.CSRF_TOKEN,
+                domain=".leetcode.com"
+            )
 
     def get_session(self) -> Session:
-        """Return the configured session."""
         return self.session
